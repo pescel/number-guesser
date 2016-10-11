fbe91014b296ad6704d950651141d620bc369aa1
pescel <28l.pesce@gmail.com>
2016-10-11 13:28:03 -0600

    add reset button

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,27 @@
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 var randomNum = getRandom(0,100);
 var guessButton = document.querySelector("[name='guess']")
+var clearButton = document.querySelector("[name='clear']");
+var resetButton = document.querySelector("[name='reset']");
+var userInput = document.querySelector("[name='userNumber']");
+
+
 guessButton.addEventListener('click', function() {
   //when guess button is clicked (triggers 'click' event) runs everything in between {}.
-  var userNumber = document.querySelector("[name='userNumber']").value;
+  var userNumber = userInput.value;
+
   userNumber = parseInt(userNumber);
   //finds input field with attribute name='userNumber' and grabs the value (number). ParseInt changes strings to intergers.
   if(isNaN(userNumber)) {
-    alert("This is not a number!");
+    return alert("This is not a number!");
   }
 
   if(userNumber > 100 || userNumber < 0) {
     alert("This number is outside the range of possible answers!")
   }
-
-  if(userNumber)
 
   document.querySelector(".pink-number").innerText = userNumber;
   //takes userNumber value and displays it as big pink numbers.
@@ -30,15 +35,26 @@ guessButton.addEventListener('click', function() {
   }
 });
 
-var clearButton = document.querySelector("[name='clear']");
+//this disables the clear button once it's clicked.
 clearButton.addEventListener('click', function() {
-  document.querySelector("[name='userNumber']").value = null;
+  userInput.value = null;
+  clearButton.disabled = true;
 })
 
- var resetButton = document.querySelector("[name='reset']");
- resetButton.addEventListener('click', function() {
-   document.querySelector("[name='userNumber']").value = null;
+//this is using the reset button to clear text box, come up with new random number, clear the pink number, and get rid of the high-low statement.
+resetButton.addEventListener('click', function() {
+   userInput.value = null;
    randomNum = getRandom(0,100);
    document.querySelector(".pink-number").innerText = null;
    document.querySelector(".high-low").innerText = null;
+ })
+
+//
+userInput.addEventListener('input', function() {
+  if(userInput.value !== "") {
+    clearButton.disabled = false;
+  } else {
+    clearButton.disabled = true;
+  }
+
  })
