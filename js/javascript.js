969aabdf91d5774532c078beaccae9a33b56805d
pescel <28l.pesce@gmail.com>
2016-10-10 18:34:34 -0600

    add basic js input in display

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,13 @@ function getRandom(min, max) {
   return Math.random() * (max-min) + min;
 }
 
+var userNumber = document.querySelector("[name='userNumber']").value;
+
+document.querySelector(".pink-number").innerText = userNumber;
+
 var randomNum = getRandom(0,100);
-if( /* user's number */ < randomNum) {
-  /* message */ 
+if(userNumber < randomNum) {
+document.querySelector(".high-low").innerText = "That is too low"
+} else if(userNumber > randomNum) {
+  document.querySelector(".high-low").innerText = "That is too high"
 }
