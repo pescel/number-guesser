38f0632558aaa8d571277a1bbf55ea5c942d2fb2
pescel <28l.pesce@gmail.com>
2016-10-11 18:42:43 -0600

    add min/max inputs

--- js/javascript.js
+++ js/javascript.js
@@ -2,12 +2,37 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// set default values
+var min = 0;
+var max = 100;
+// set the variable randomNum to the result of running the function getRandom
 var randomNum = getRandom(0,100);
-var guessButton = document.querySelector("[name='guess']")
+
+// select DOM elements and set to variables
+var guessButton = document.querySelector("[name='guess']");
 var clearButton = document.querySelector("[name='clear']");
 var resetButton = document.querySelector("[name='reset']");
 var userInput = document.querySelector("[name='userNumber']");
+var minInput = document.querySelector("[name='min']");
+var maxInput = document.querySelector("[name='max']");
 
+// take the minInput field and wait for the user to change what's in the box
+minInput.addEventListener('input', function() {
+  // when the user changes whats in the box, run this function
+  min = minInput.value
+  // min now equals the user's inputted min value
+  min = parseInt(min)
+  // have to change the input to an integer because its stupid and comes to us as a string
+  randomNum = getRandom(min,max)
+  // then we reassign the randomNum by running the function getRandom with the new min integer
+});
+
+// same thing but with max
+maxInput.addEventListener('input', function() {
+  max = maxInput.value
+  max = parseInt(max)
+  randomNum = getRandom(min,max)
+});
 
 guessButton.addEventListener('click', function() {
   //when guess button is clicked (triggers 'click' event) runs everything in between {}.
@@ -44,6 +69,9 @@ clearButton.addEventListener('click', function() {
 //this is using the reset button to clear text box, come up with new random number, clear the pink number, and get rid of the high-low statement.
 resetButton.addEventListener('click', function() {
    userInput.value = null;
+   // we have to change the input of the min/max box (element) to the value of 0/100 when the reset button is clicked
+   minInput.value = 0;
+   maxInput.value = 100;
    randomNum = getRandom(0,100);
    document.querySelector(".pink-number").innerText = null;
    document.querySelector(".high-low").innerText = null;
@@ -58,7 +86,3 @@ userInput.addEventListener('input', function() {
   }
 
  })
-
- for (i = 0; i < array.length; i++) {
-   array[i]
- }
